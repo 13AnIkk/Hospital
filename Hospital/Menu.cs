619d61aa039919453eebc2dd1a3bc7e9dee4b490
13AnIkk <robson.banik@gmail.com>
2022-04-11 23:00:07 -0300

    por favor para

---
+++ Hospital/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Menu : Paciente
+    {
+        public void menu()
+        {
+            Console.WriteLine("\nO que deseja fazer?\n\n1-Cadastrar paciente\n2-Listar pacientes\n3-Atenter pacientes\n4-Alterar dados de um paciente\nQ-Fechar programa\n");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Paciente paciente = new Paciente();
+                    paciente.paciente();
+                    break;
+                case "2":
+                    
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "Q":
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Hospital/Paciente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    public class Paciente : Program
+    {
+        public string digitado;
+        public bool[] sexo = new bool[9];
+        public string[] nome = new string[9];
+        public int[] idade = new int[9];
+        public bool[] prioridade = new bool[9];
+        public string[] cpf = new string[9];
+        public void paciente()
+        {
+            Console.WriteLine("\nO indivíduo tem prioridade?");
+            digitado = Console.ReadLine();
+
+            if (digitado == "Sim")
+            {
+                prioridade[0] = true;
+            }
+            else if (digitado == "não")
+            {
+                prioridade[0] = false;
+            }
+            Console.WriteLine("\nQual o indivíduo é do sexo Feminino ou Masculino?");
+            digitado = Console.ReadLine();
+
+            if (digitado == "Masculino")
+            {
+                sexo[0] = true;
+            }
+            else if (digitado == "Feminino")
+            {
+                sexo[0] = false;
+            }
+
+            Console.WriteLine("\nQual nome do indivíduo?");
+            nome[0] = Console.ReadLine();
+
+            Console.WriteLine("\nQual CPF do indivíduo?");
+            cpf[0] = Console.ReadLine();
+
+            Menu menu = new Menu();
+            menu.menu();
+        }
+
+    }
+    
+}
+
